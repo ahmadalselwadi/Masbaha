9fa521c0ce0849f23380c2db9e2e7e24465149ed
Arak <ahmad@Ahmads-MacBook-Air.local>
2015-04-29 11:48:56 +0300

    Initial Commit

---
+++ Masbaha/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Masbaha
+//
+//  Created by Arak on 4/29/15.
+//  Copyright (c) 2015 Ahmad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Masbaha/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Masbaha
+//
+//  Created by Arak on 4/29/15.
+//  Copyright (c) 2015 Ahmad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
